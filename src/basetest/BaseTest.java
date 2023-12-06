5ca79e4dec4e569a01e671a4f81d0d35586a4a30
[name] <destiny.creator1959@gmail.com>
2023-12-06 23:20:36 +0000

    para

---
+++ src/basetest/BaseTest.java
@@ -0,0 +1,25 @@
+package basetest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public static WebDriver driver;
+
+    public void openBrowser(String url) {
+        ChromeOptions options = new ChromeOptions();// setting webdriver
+        //options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get(url);// launch url
+        driver.manage().window().maximize(); // maximising screen
+        driver.manage().timeouts().implicitlyWait( Duration.ofSeconds(2000)); // giving wait
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+}
---
+++ src/utility/Utility.java
@@ -0,0 +1,37 @@
+package utility;
+
+import basetest.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class Utility extends BaseTest {
+    public void clickOnElement(By by) {
+        WebElement element = driver.findElement(by);
+        element.click();
+    }
+
+    //This method will get text
+    public String getTextFromElement(By by) {
+        return driver.findElement(by).getText();// logout
+
+    }
+
+    // This method will send text to elements
+    public void sendTextToElement(By by, String text) {
+        WebElement element=driver.findElement(by);
+        element.sendKeys(text);
+    }
+
+    // This method will send text to element
+    public void sendKeysToElement(By by, CharSequence keys) {
+        driver.findElement(by).sendKeys(keys);
+    }
+
+    // This method will clear text from field
+
+    public void clearTextFromField(By by) {
+        driver.findElement(by).sendKeys( Keys.CONTROL + "a");
+        driver.findElement(by).sendKeys(Keys.DELETE);
+    }
+}
